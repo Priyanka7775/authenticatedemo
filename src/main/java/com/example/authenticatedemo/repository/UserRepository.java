5e6a30b2ac3f97f97ac4e45c14785da6e6142694
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-11-15 17:22:41 +0530

    added docker files

--- src/main/java/com/example/authenticatedemo/repository/UserRepository.java
+++ src/main/java/com/example/authenticatedemo/repository/UserRepository.java
@@ -4,7 +4,5 @@ import com.example.authenticatedemo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-
-
     public User findByUsernameAndPassword(String username, String password);
 }
